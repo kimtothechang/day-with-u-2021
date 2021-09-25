45b7c16071d32edba4d0207cdfff9b9b0e91a0e0
kimtothechang <88170881+kimtothechang@users.noreply.github.com>
2021-09-26 04:44:25 +0900

    CREATE app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  const username = loginInput.value;
+  event.preventDefault();
+  console.log(username);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
