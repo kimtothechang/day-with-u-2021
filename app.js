4a4980f1493d4031e23d1cde22363d6461624e98
kimtothechang <88170881+kimtothechang@users.noreply.github.com>
2021-09-27 02:40:29 +0900

    UPDATE app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  event.preventDefault();
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+  paintGreetings(savedUsername);
+}
