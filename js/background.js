514f8ea4ddaddd495ba2f35e78faca6dee7c81d9
kimtothechang <88170881+kimtothechang@users.noreply.github.com>
2021-10-01 06:54:02 +0900

    UPDATE background.js

--- js/background.js
+++ js/background.js
@@ -1,10 +1,37 @@
-const images = ["0.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg","9.jpeg"];
+const images = [
+  "0.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+  "10.jpeg",
+  "11.jpeg",
+  "12.jpeg",
+  "13.jpeg",
+  "14.jpeg",
+  "15.jpeg",
+  "16.jpeg",
+  "17.jpeg",
+  "18.jpeg",
+  "19.jpeg",
+];
 
+const chosenImage = images[10 + Math.floor(Math.random() * 9)];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.querySelector("#body");
 
+body.style = `background-image: url("img/${chosenImage}")`;
+
+//Momentum-App-Clone-Coding-Script
+/*
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.id = `bgImage`;
 
 document.body.appendChild(bgImage);
+*/
