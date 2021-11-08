db8d39ad0d48da929cb5b0b7cc55587c90d57580
kimtothechang <88170881+kimtothechang@users.noreply.github.com>
2021-11-08 21:44:50 +0900

    UPDATE background.js

--- js/background.js
+++ js/background.js
@@ -3,7 +3,6 @@ const images = ["0.jpeg", "1.png", "2.jpeg", "3.jpg", "4.jpeg"];
 const chosenImage = images[0 + Math.floor(Math.random() * 4)];
 
 const bgImage = document.querySelector("#body");
-const fontColor = document.querySelector("#body");
 
 body.style = `background-image: url("img/${chosenImage}")`;
 
